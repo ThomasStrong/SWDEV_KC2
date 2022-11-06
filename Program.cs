0fecd5fd1d27e5250cb42958876cdc82bd1c8ec4
Tom Strong <strng_thms@yahoo.com>
2022-11-06 09:21:40 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SWDEV_KC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
